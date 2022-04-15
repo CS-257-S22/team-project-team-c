ac225a74a2778f35e81ced0ba325837a95dd05e2
Kana Hashimoto <103124342+hashimotok2001@users.noreply.github.com>
2022-04-14 21:47:52 -0500

    Update what2Eat.py

--- productionCode/what2Eat.py
+++ productionCode/what2Eat.py
@@ -1,8 +1,7 @@
-"""This code is a tes"""
-# argv.py
 import string
 import sys 
-def getBrandName ():
+import csv 
+"""def getBrandName ():
     argList = sys.argv
 
     if (len(argList) == 2 ):
@@ -13,5 +12,38 @@ def getBrandName ():
        print("you have entered the wrong arguments bestie")
        return  
 
-brandName = getBrandName()
+brandName = getBrandName()"""
+
+'''
+Load information from a .csv file
+Input: file name (should be .csv format)
+Output: a nested 2D list of product data 
+'''
+def load_csv_file(filename):
+    productData = [] #contains product name, brand, and ingredients 
+    with open(filename, 'r') as data:
+        for line in csv.reader(data):
+            productData.append(line)
+
+    return productData
+
+
+def getAllProducts(brandName):
+    # get a brand name from the user (calls getUserBrandInput)
+    productData = load_csv_file("SmallProductSheet.csv")
+    productList = []
+    for line in productData: 
+        if line[1] == brandName:
+            productList.append(line[0])
+    return productList
+
+#getAllProducts("FRESH & EASY")
+
+
+
+def getProductIngredients(brandName, productName):
+   #load csv file
+   # isolate by brand
+   # isolate by product
+   # return ingredient list  
 
