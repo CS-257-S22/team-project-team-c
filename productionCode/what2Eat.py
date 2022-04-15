8466ff9ce4bc7ba2083b72d00b06a5110656d058
Isabella <63384098+chaisabella@users.noreply.github.com>
2022-04-14 22:50:23 -0500

    Update what2Eat.py

--- productionCode/what2Eat.py
+++ productionCode/what2Eat.py
@@ -1,23 +1,10 @@
 import string
 import sys 
 import csv 
-"""def getBrandName ():
-    argList = sys.argv
 
-    if (len(argList) == 2 ):
-        print("you have entered correct brand nums!")
-        brandName = argList[1]
-        return brandName  
-    else: 
-       print("you have entered the wrong arguments bestie")
-       return  
-
-brandName = getBrandName()"""
-
-'''
-Load information from a .csv file
+'''Loads information from a .csv file. Each row is represented as a list within a larger list.
 Input: file name (should be .csv format)
-Output: a nested 2D list of product data 
+Return: a nested 2D list of product data 
 '''
 def load_csv_file(filename):
     productData = [] #contains product name, brand, and ingredients 
@@ -27,22 +14,86 @@ def load_csv_file(filename):
 
     return productData
 
-
-
-
-def getAllProducts(brandName):
-    # get a brand name from the user (calls getUserBrandInput)
+'''
+Helper method to return all brands as a list
+'''
+def returnBrands():
     productData = load_csv_file("SmallProductSheet.csv")
-    productList = []
-    for line in productData: 
-        if line[1] == brandName:
-            productList.append(line[0])
-    return productList
-    
-print(getAllProducts("FRESH & EASY"))
+    allBrands = []
 
-# def getProductIngredients(brandName, productName):
-   #load csv file
-   # isolate by brand
-   # isolate by product
-   # return ingredient list  
+    for item in productData:
+        brand = item[1]
+        if brand not in allBrands and brand: 
+            allBrands.append(brand)
+
+    allBrands.remove('brand_name') #remove the column name 
+
+    return allBrands 
+
+'''
+Helper method to display (print) all brands as a list
+'''
+def displayBrands():
+    productData = load_csv_file("SmallProductSheet.csv")
+    allBrands = []
+
+    for item in productData:
+        brand = item[1]
+        if brand not in allBrands and brand: 
+            allBrands.append(brand)
+
+    allBrands.remove('brand_name') #remove the column name 
+
+    print(allBrands) 
+
+'''
+Helper method to check if user input is a valid brand 
+'''
+def isValidBrand(brandName):
+    allBrands = returnBrands()
+    if brandName in allBrands:
+        return True
+    else: 
+        print("The brand is non-existent or mispelled. The list of brands are: ") #Error message 
+        displayBrands()
+        return False
+
+
+'''
+Input: brand name 
+Returns: a list of all products carried by the given brand. 
+'''
+def getAllProducts(brandName):
+
+    if isValidBrand(brandName):
+        productData = load_csv_file("SmallProductSheet.csv") #product data continaing name, brand, and ingredients 
+        productList = []
+
+        for item in productData: 
+            if item[1] == brandName:
+                productList.append(item[0])
+
+        return productList
+
+
+'''
+Input: brand and product
+Returns: the list of ingredients 
+'''
+def getProductIngredients(brandName, productName):
+    productData = load_csv_file("SmallProductSheet.csv") #product data continaing name, brand, and ingredients 
+    
+    for item in productData:
+        if item[0] == productName and item[1] == brandName:
+            print(item[2])
+    
+    
+
+
+
+# def main():
+#     #TESTING STUFF 
+#     #print(getAllProducts("FRESH & EASY"))
+# getProductIngredients('FRESH & EASY', 'BARBECUE SAUCE')
+
+
