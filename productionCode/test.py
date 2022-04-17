5f5cc5ca619b16e291d28492e5981b6edae8ccd4
Alice  Cutter <100951218+Alice-Cutter@users.noreply.github.com>
2022-04-17 15:00:02 -0500

    copied test.py
    copied test.py from the outside folder into this one.

--- productionCode/test.py
+++ productionCode/test.py
@@ -1,6 +1,5 @@
 import unittest
-import what2Eat
-
+import what2eat
 
 class TestWhat2Eat(unittest.TestCase):
     def test_compareNutrients(self):
@@ -10,47 +9,32 @@ class TestWhat2Eat(unittest.TestCase):
 
         self.assertEqual(nutrients, providedNutrients)
         
-
 class TestHasIngredients(unittest.TestCase):
     def test_has_ingredient(self):
         """Testing the function hasIngredients
         written by Isabella """
         result = hasIngredient("garlic".upper(), "Roasted Garlic Hummus", "Target")
         self.assertEqual(result, True)
+class TestLoadCSVFile(unittest.TestCase):
+    def test_load_csv_file(self):
+        """Testing the function loadCSVFile written by Alice"""
+        productData = load_csv_file("DummyData.csv")
+        testData = ["MOCHI ICE CREAM BONBONS","G. T. Japan", "Inc.","MILK, CREAM, SUGAR"]
+       
+        self.assertEqual(productData, testData)
 
+class TestValidBrandName(unittest.TestCase):
+    def test_is_Valid_Brand_True(self):
+        """Testing the function is_Valid_Brand written by Alice. This test takes in real brand and isValidBrand is expected to
+        return true. """
+       
+        validBrandBool = isValidBrand("G. T. Japan, Inc")
+        self.assertEqual(True,validBrandBool)
 
-class TestGetAllProducts(unittest.TestCase):
-    # def _write_temp_csv_file(self):  # helper method
-    #     pass
+    def test_is_Valid_Brand_False(self):
+        """Testing the function is_Valid_Brand written by Alice. This test makes
+         sure that method returns false when the brand is invalid"""
+        invalidBrandBool = validBrand("G. T. China, Inc")
+        self.assertEqual(invalidBrandBool, False)
 
-    def test_returnBrands(self):
-        """Testing the helper function returnBrands. The test function uses a small""" 
-        """sample of the dataset to compare results"""
-        """Written by Morgan"""
-
-        brands = what2Eat.returnBrands("dataForTesting.csv")
-        accurateListOfBrands = ["G. T. Japan, Inc.","FRESH & EASY"]
-        self.assertEqual(brands, accurateListOfBrands)
-
-    def test_returnBrandsWithEmptyFile(self):
-        """Testing the returnBrands handling of an empty file"""
-        """Written by Morgan"""
-        brands = what2Eat.returnBrands("productionCode/testData/emptyFileForTesting.csv")
-        self.assertEqual([], brands)
-
-    def test_returnBrandsFileNotFound(self):
-        with self.assertRaises(FileNotFoundError):
-            brands = what2Eat.returnBrands('')
-   
-    '''
-    ideas:
-        returnBrands should return brands (not ingredients, etc.)
-        returnBrands should not return any "empty" brands if they exist in the CSV file.
-        returnBrands should not return the same brand more than once (even if it appears twice in teh CSV file.)
-        returnBrands returns brands in the order that they are found
-        returnBrands does not include column label 
-    '''
-
-
-if __name__ == '__main__':
-    unittest.main()
+        
