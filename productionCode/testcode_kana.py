0b1a22f54384c47e18fa0bef81a00a09fd53d5a2
hashimotok_2001 <hashimotok@carleton.edu>
2022-04-18 21:57:45 -0500

    documentation

--- productionCode/testcode_kana.py
+++ productionCode/testcode_kana.py
@@ -4,14 +4,17 @@ from what2Eat import *
 
 class TestIngredient(unittest.TestCase):
     def setUp(self):
+        """Set the file used to test functions"""
         self.sampleData = what2Eat("SmallProductSheet.csv")
 
     def test_load_csv_file(self):
+        """Testing if the function load_csv_file returns a nested list"""
         result =self.sampleData.load_csv_file()
         ifNested = any(isinstance(row, list) for row in result)
         self.assertTrue(ifNested)
 
     def test_returnBrands(self):
+        """Testing if returnBrands returns all brands correctly"""
         result =self.sampleData.returnBrands()
         brandList = ['G. T. Japan, Inc.', 'FRESH & EASY', "McConnell's Ice Cream Inc.",
          'Stater Bros. Markets Inc.', 'Meijer, Inc.', 'Target Stores', 
@@ -21,21 +24,22 @@ class TestIngredient(unittest.TestCase):
         self.assertEqual(result, brandList)
 
     def test_isValidBrand_True(self):
+        """Testing if isValidBrand returns True when getting a correct brand name"""
         result =self.sampleData.isValidBrand("FRESH & EASY")
         self.assertTrue(result)
 
     def test_isValidBrand_False(self):
+        """Testing if isValidBrand returns False when getting a nonexisting brand name"""
         result =self.sampleData.isValidBrand("FRESH OR EASY")
         self.assertFalse(result)
 
     def test_isValidBrand_blank(self):
+        """Testing if isValidBrand returns False when getting a blank as input"""
         result =self.sampleData.isValidBrand("")
         self.assertFalse(result)
 
-        #number
-        #main + edge (blank)
-
     def test_getAllProducts(self):
+        """Testing if getAllProducts returns all products when getting a correct brand name"""
         result =self.sampleData.getAllProducts("Stater Bros. Markets Inc.")
         allProducts = ['STATER BROS., SUGAR POWDERED','STATER BROS., PURE CANE SUGAR GRANULATED',
                        'STATER BROS., ROTINI AN ENRICHED MACARONI PRODUCT',
@@ -43,35 +47,48 @@ class TestIngredient(unittest.TestCase):
         self.assertEqual(result, allProducts)
 
     def test_getProductIngredients(self):
-            result =self.sampleData.getProductIngredients("FRESH & EASY", "BARBECUE SAUCE")
-            allIngredients = "tomato puree (water, tomato paste), sugar, distilled vinegar, molasses, water, modified corn starch, salt, bourbon whiskey, contains 1% or less of: mustard flour, spice, dried onion, dried garlic, natural flavor, xanthan gum, caramel color."
-            self.assertEqual(result, allIngredients)
+        """Testing if getProductIngredients returns correct ingredients when getting correct product and brand names"""
+        result =self.sampleData.getProductIngredients("FRESH & EASY", "BARBECUE SAUCE")
+        allIngredients = "tomato puree (water, tomato paste), sugar, distilled vinegar, molasses, water, modified corn starch, salt, bourbon whiskey, contains 1% or less of: mustard flour, spice, dried onion, dried garlic, natural flavor, xanthan gum, caramel color."
+        self.assertEqual(result, allIngredients)
+
+    def test_getProductIngredients_falseCombination(self):
+        """Testing if getProductIngredients returns None when getting false combination of existing product and brand names"""
+        result =self.sampleData.getProductIngredients("Target Stores", "BARBECUE SAUCE")
+        self.assertEqual(result, None)
 
     def test_containIngredients(self):
-            result =self.sampleData.containsIngredient("molasses", "FRESH & EASY", "BARBECUE SAUCE")
-            self.assertTrue(result)
+        """Testing if containIngredients returns True when getting appropriate input"""
+        result =self.sampleData.containsIngredient("molasses", "FRESH & EASY", "BARBECUE SAUCE")
+        self.assertTrue(result)
     
     def test_containIngredients_FalseIngredients(self):
-            result =self.sampleData.containsIngredient("random product", "FRESH & EASY", "BARBECUE SAUCE")
-            self.assertFalse(result)
+        """Testing if containIngredients returns alse when getting a wrong ingredient name"""
+        result =self.sampleData.containsIngredient("random product", "FRESH & EASY", "BARBECUE SAUCE")
+        self.assertFalse(result)
     
     def test_containIngredients_FalseBrand(self):
-            result =self.sampleData.containsIngredient("molasses", "Random Brand", "BARBECUE SAUCE")
-            self.assertFalse(result)
+        """Testing if containIngredients returns alse when getting a wrong brand name"""
+        result =self.sampleData.containsIngredient("molasses", "Random Brand", "BARBECUE SAUCE")
+        self.assertFalse(result)
     
     def test_containIngredients_FalseProduct(self):
-            result =self.sampleData.containsIngredient("molasses", "FRESH & EASY", "Random Product")
-            self.assertFalse(result)
+        """Testing if containIngredients returns alse when getting a wrong product name"""
+        result =self.sampleData.containsIngredient("molasses", "FRESH & EASY", "Random Product")
+        self.assertFalse(result)
 
     def test_brandCarriesProduct_True(self):
+        """Testing if brandCarriesProduct True when getting appropriate input"""
         result =self.sampleData.brandCarriesProduct('FRESH & EASY', 'BARBECUE SAUCE')
         self.assertTrue(result)
 
     def test_brandCarriesProduct_FalseBrand(self):
+        """Testing if brandCarriesProduct True when getting a wrong brand name"""
         result =self.sampleData.brandCarriesProduct('Random Brand', 'BARBECUE SAUCE')
         self.assertFalse(result)
     
     def test_brandCarriesProduct_FalseProduct(self):
+        """Testing if brandCarriesProduct True when getting a wrong product name"""
         result =self.sampleData.brandCarriesProduct('FRESH & EASY', 'Random Product')
         self.assertFalse(result)
 
