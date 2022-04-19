146f13960ba8475f437b494455f922a098a10723
hashimotok_2001 <hashimotok@carleton.edu>
2022-04-18 21:57:15 -0500

    add Kana's tests

--- productionCode/test.py
+++ productionCode/test.py
@@ -145,6 +145,13 @@ class TestWhat2Eat(unittest.TestCase):
         Written by Isabella"""
         result = self.defaultSampleData.containsIngredient("chickpeas", "Target Stores", "ROASTED RED PEPPER HUMMUS")
         self.assertTrue(result)
+    
+    def test_getProductIngredients(self):
+        """Testing if getProductIngredients returns correct ingredients when getting correct product and brand names
+        Written by Kana"""
+        result =self.defaultSampleData.getProductIngredients("FRESH & EASY", "BARBECUE SAUCE")
+        allIngredients = "tomato puree (water, tomato paste), sugar, distilled vinegar, molasses, water, modified corn starch, salt, bourbon whiskey, contains 1% or less of: mustard flour, spice, dried onion, dried garlic, natural flavor, xanthan gum, caramel color."
+        self.assertEqual(result, allIngredients)
 
     def test_getIngredients_from_empty_file(self):
         """Testing if the function getProductIngredients returns None when attempting to retreive ingredients from an empty file 
@@ -158,7 +165,68 @@ class TestWhat2Eat(unittest.TestCase):
         ingredients = self.defaultSampleData.getProductIngredients("family fare", "barbecue sauce")
         self.assertEqual(ingredients, None)
 
+    def test_getProductIngredients_falseCombination(self):
+        """Testing if getProductIngredients returns None when getting false combination of existing product and brand names
+        Written by Kana"""
+        result =self.defaultSampleData.getProductIngredients("Target Stores", "BARBECUE SAUCE")
+        self.assertEqual(result, None)
+
+    def test_isValidBrand_blank(self):
+        """Testing if isValidBrand returns False when getting a blank as input
+        Written by Kana"""
+        result =self.defaultSampleData.isValidBrand("")
+        self.assertFalse(result)
+
+    def test_getAllProducts(self):
+        """Testing if getAllProducts returns all products when getting a correct brand name
+        Written by Kana"""
+        result =self.defaultSampleData.getAllProducts("Stater Bros. Markets Inc.")
+        allProducts = ['STATER BROS., SUGAR POWDERED','STATER BROS., PURE CANE SUGAR GRANULATED',
+                       'STATER BROS., ROTINI AN ENRICHED MACARONI PRODUCT',
+                       'STATER BROS., KIDNEY BEANS, DARK RED']
+        self.assertEqual(result, allProducts)
+
+    def test_containIngredients(self):
+        """Testing if containIngredients returns True when getting appropriate input
+        Written by Kana"""
+        result =self.defaultSampleData.containsIngredient("molasses", "FRESH & EASY", "BARBECUE SAUCE")
+        self.assertTrue(result)
     
+    def test_containIngredients_FalseIngredients(self):
+        """Testing if containIngredients returns alse when getting a wrong ingredient name
+        Written by Kana"""
+        result =self.defaultSampleData.containsIngredient("random product", "FRESH & EASY", "BARBECUE SAUCE")
+        self.assertFalse(result)
+    
+    def test_containIngredients_FalseBrand(self):
+        """Testing if containIngredients returns alse when getting a wrong brand name
+        Written by Kana"""
+        result =self.defaultSampleData.containsIngredient("molasses", "Random Brand", "BARBECUE SAUCE")
+        self.assertFalse(result)
+    
+    def test_containIngredients_FalseProduct(self):
+        """Testing if containIngredients returns alse when getting a wrong product name
+        Written by Kana"""
+        result =self.defaultSampleData.containsIngredient("molasses", "FRESH & EASY", "Random Product")
+        self.assertFalse(result)
+
+    def test_brandCarriesProduct_True(self):
+        """Testing if brandCarriesProduct True when getting appropriate input
+        Written by Kana"""
+        result =self.defaultSampleData.brandCarriesProduct('FRESH & EASY', 'BARBECUE SAUCE')
+        self.assertTrue(result)
+
+    def test_brandCarriesProduct_FalseBrand(self):
+        """Testing if brandCarriesProduct True when getting a wrong brand name
+        Written by Kana"""
+        result =self.defaultSampleData.brandCarriesProduct('Random Brand', 'BARBECUE SAUCE')
+        self.assertFalse(result)
+    
+    def test_brandCarriesProduct_FalseProduct(self):
+        """Testing if brandCarriesProduct True when getting a wrong product name
+        Written by Kana"""
+        result =self.defaultSampleData.brandCarriesProduct('FRESH & EASY', 'Random Product')
+        self.assertFalse(result)
 
 if __name__ == '__main__':
     unittest.main()
