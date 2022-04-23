50b12ff085f7ee9d0891cad277de19670495ad7e
CS Lab Account <cslab@olin308-09.mathcs.carleton.edu>
2022-04-23 14:57:09 -0500

    Isabella's tests

--- productionCode/flaskapp_test.py
+++ productionCode/flaskapp_test.py
@@ -9,24 +9,32 @@ class TestTwoMainFunction(unittest.TestCase):
         self.app = app.test_client()
         
     def test_get_ingredients(self):
-        '''Retreiving the ingreident list from 
+        '''
+        @Written by: Isabella
+        Retreiving the ingreident list from 
         valid inputs for product and brand name.'''
-        response = self.app.get('/getProductIngredients/Target Stores/TRADITIONAL HUMMUS', follow_redirects=True)
+        response = self.app.get('/get_product_ingredients/Target Stores/TRADITIONAL HUMMUS', follow_redirects=True)
         self.assertEqual(b'TRADITIONAL HUMMUS from Target Stores contains: chickpeas, water, sesame tahini, canola/olive oil blend, salt, citric acid, natural flavor, garlic, spices, potassium sorbate and sodium benzoate (to maintain freshness).', response.data)
     
     def test_invalid_product_get_ingredients(self):
-        '''Edge case: user inputs invalid product name'''
-        response = self.app.get('/getProductIngredients/Target Stores/HUMMUS', follow_redirects=True)
+        '''
+        @Written by: Isabella
+        Edge case: user inputs invalid product name'''
+        response = self.app.get('/get_product_ingredients/Target Stores/HUMMUS', follow_redirects=True)
         self.assertEqual(b'HUMMUS from Target Stores contains: None', response.data)
     
     def test_invalid_brand_get_ingredients(self):
-        '''Edge case: user inputs invalid brand name'''
-        response = self.app.get('/getProductIngredients/Target/TRADITIONAL HUMMUS', follow_redirects=True)
+        '''
+        @Written by: Isabella
+        Edge case: user inputs invalid brand name'''
+        response = self.app.get('/get_product_ingredients/Target/TRADITIONAL HUMMUS', follow_redirects=True)
         self.assertEqual(b'TRADITIONAL HUMMUS from Target contains: None', response.data)
     
     def test_contains_ingredient(self):
-        '''Checking if a product contains ingreident'''
-        response = self.app.get('/containsIngredient/chickpeas/Target Stores/TRADITIONAL HUMMUS', follow_redirects=True)
+        '''
+        @Written by: Isabella
+        Checking if a product contains ingreident'''
+        response = self.app.get('/contains_ingredient/chickpeas/Target Stores/TRADITIONAL HUMMUS', follow_redirects=True)
         print(response.data)
         self.assertEqual(b'TRADITIONAL HUMMUS from Target Stores contains chickpeas', response.data)
 
