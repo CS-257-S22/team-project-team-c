4fd4652c50e152e979098a8e2d286f22f7859ea8
CS Lab Account <cslab@olin308-09.mathcs.carleton.edu>
2022-04-23 14:56:13 -0500

    changed method names to have _

--- productionCode/flaskapp.py
+++ productionCode/flaskapp.py
@@ -8,25 +8,44 @@ app = Flask(__name__)
 sampleData = ProductData('SmallProductSheet.csv')
 data = []
 @app.route('/')
+
 def homepage():
-    return "hello, this is the homepage"
-    #http://127.0.0.1:5000/
+    """ Generate a homepage
+    @return a homepage with instructions
+    """ 
+    return "You can find all the ingredients of a product by going to http://127.0.0.1:5000/getProductIngredients/[brandName]/[productName]. \
+            \n Here is an example: \
+            \n http://127.0.0.1:5000/getProductIngredients/DCI Cheese Company, Inc./GREAT MIDWEST, CRANBERRY CHEDDAR \
+            \n \
+            \n You can also find all products carried by a certain brand by going to http://127.0.0.1:5000/getAllProducts/[brandName] \
+            \n Here is an example: \
+            \n http://127.0.0.1:5000/getAllProducts/Target Stores"
     
-@app.route('/containsIngredients/<ingredient>/<brandName>/<productName>', strict_slashes=False)
-def check_one_ingredient(ingredient, brandName, productName):
-    if sampleData.containsIngredient(ingredient, brandName, productName): 
-        return f'{productName} from {brandName} contains {ingredient}'
-        
+@app.route('/getAllProducts/<brandName>', strict_slashes=False)
+def display_products(brandName):
+    """ Generate a page that returns ingredients
+    @param 
+        brandName: a brand name that a user want to search with
+    @return a list of all products carried by the brand
+    """ 
+    return f'{brandName} carries: {sampleData.getAllProducts(brandName)}'
+    #examples:
+    #return str(sampleData.getProductIngredients(brandName, productName))
+    #http://127.0.0.1:5000/getAllProducts/FRESH%20&%20EASY
+
 @app.route('/getProductIngredients/<brandName>/<productName>', strict_slashes=False)
 def display_ingredients(brandName, productName):
     return f'{productName} from {brandName} contains: {sampleData.getProductIngredients(brandName, productName)}'
     #http://127.0.0.1:5000/getProductIngredients/FRESH%20&%20EASY/BARBECUE%20SAUCE
 
-@app.route('/containsIngredients/<ingredient>/<brandName>/<productName>', strict_slashes=False)
-def check_one_ingredient(ingredient, brandName, productName):
+
+@app.route('/containsIngredient/<ingredient>/<brandName>/<productName>', strict_slashes=False)
+def display_contains_ingredient(ingredient, brandName, productName):
     if sampleData.containsIngredient(ingredient, brandName, productName): 
         return f'{productName} from {brandName} contains {ingredient}'
-
+    else: 
+        return f'{productName} from {brandName} does not contain {ingredient}' 
+        
 @app.errorhandler(404)
 def page_not_found(e):
     return "sorry, wrong format..."
