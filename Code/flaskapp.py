942f6901fefbcee9bda2d316a92136147efe52ad
chaisabella <chaisabella@carleton.edu>
2022-05-30 09:13:56 -0500

    update contact page

--- Code/flaskapp.py
+++ Code/flaskapp.py
@@ -9,16 +9,6 @@ from datasource import *
 
 app = Flask(__name__)
 database = DataSource()
-helpMessage = "You can find all the ingredients of a product by going to http://127.0.0.1:5000/get_product_ingredients/[brandName]/[productName]. \
-            \n Here is an example: \
-            \n http://127.0.0.1:5000/get_product_ingredients/DCI Cheese Company, Inc./GREAT MIDWEST, CRANBERRY CHEDDAR \
-            \n \
-            \n You can also find all products carried by a certain brand by going to http://127.0.0.1:5000/get_all_products/[brandName] \
-            \n Here is an example: \
-            \n http://127.0.0.1:5000/get_all_products/Target Stores \
-            \n You can check if a product contains something by going to http://127.0.0.1:5000/contains_ingredient/[ingredient]/[brandName]/[productName]\
-            \n Here is an example: \
-            \n http://127.0.0.1:5000/contains_ingredient/chickpeas/Target Stores/TRADITIONAL HUMMUS"
 
 @app.route('/')
 def homepage():
@@ -29,14 +19,20 @@ def homepage():
 
 @app.route('/contact')
 def contact():
+    """ Generate a contact page
+    """
     return render_template('contact.html')
 
 @app.route('/help')
 def help():
+    """ Generate a help page
+    """
     return render_template('help.html')
 
 @app.route('/contact/contact_submitted')
 def contact_submitted():
+    """ Let users submit a contact form
+    """
     return render_template('contact_submitted.html')
     
 @app.route('/displayproducts', methods=['POST'])
@@ -45,7 +41,7 @@ def display_products():
     @return a page with a list of all products carried by the brand
     """ 
     brand = (request.form['brandSearch'])
-    return render_template('products.html', brand=brand, products=database.get_all_products(brand))
+    return render_template('products.html', brand=brand, products=database.get_all_products_from_brand(brand))
 
 @app.route('/displayingredients', methods=['POST'])
 def display_ingredients():
@@ -53,7 +49,7 @@ def display_ingredients():
     @return a page with a list of all ingredients of the product
     """ 
     product = (request.form['product'])
-    return render_template('ingredients.html', product=product, ingredients=database.get_all_ingredients(product))
+    return render_template('ingredients.html', product=product, ingredients=database.get_all_ingredients_from_product(product))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -75,4 +71,4 @@ def python_bug(e):
 
 if __name__ == '__main__':
     # app.run()
-    app.run(host='127.0.0.1', port=5002)
+    app.run(host='127.0.0.1', port=5003)
